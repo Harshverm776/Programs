aeb0070f413e1412bbcd7791609f09606160a947
Harsh verma <47974733+Harshverm776@users.noreply.github.com>
2019-06-29 19:54:17 +0530

    Add files via upload

---
+++ C++ Programs/Basic/Check numbers are equal or not without ==/Equal/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter two no.s\n";
+    cin>>a>>b;
+    if(a-b)
+        cout<<"no.s are not equal";
+    else
+        cout<<"no.s are equal";
+    return 0;
+}
